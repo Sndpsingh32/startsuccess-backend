0f5f790cd3aab55e1ec1362cc9d1c60f39f21baa
sandeep singh <sndp.singh0732@gmail.com>
2026-06-03 19:14:09 +0530

    fixed

--- dist/main.js
+++ dist/main.js
@@ -58,7 +58,7 @@ async function bootstrap() {
     const document = swagger_1.SwaggerModule.createDocument(app, swaggerConfig);
     swagger_1.SwaggerModule.setup('api/docs', app, document);
     const port = process.env.PORT || 3000;
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
     console.log(`API http://localhost:${port}  Swagger /api/docs`);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   // eslint-disable-next-line no-console
   console.log(`API http://localhost:${port}  Swagger /api/docs`);
 }
